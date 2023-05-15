16063c01bb037b3ee719221033c07fe36d362010
choo121600 <choo121600@gmail.com>
2023-05-15 13:22:25 +0900

    Feat: Timeline

--- Sources/IOSIGAWeb/Theme/CustomComponents/MainContainer/TimeTable/TimeTable.swift
+++ Sources/IOSIGAWeb/Theme/CustomComponents/MainContainer/TimeTable/TimeTable.swift
@@ -23,56 +23,118 @@ struct TimeTable: Component {
             }.class("sub-sec-title")
             Div {
                 Div {
-                    Paragraph("07.08. Sat")
+                    Image("/static/images/main/Program.png")
                 }.class("session-header")
-                Table(
-                    rows: {
-                        TableRow {
-                            TableCell("14:00 ~ 14:10").class("session-time")
-                            TableCell("Opening & Welcome Speech / 소낙").class("session-title")
+                Div {
+                    Table(
+                        rows: {
+                            TableRow {
+                                TableCell("14:00 ~ 14:10").class("session-time")
+                                TableCell{
+                                    Div {
+                                        Span("오프닝 | 소낙").class("session-title")
+                                    }.class("session-info")
+                                }.class("session-describe")
+                            }
+                            TableRow {
+                                TableCell("14:10 ~ 14:40").class("session-time")
+                                TableCell{
+                                    Div {
+                                        Span("기타치다가 Swift 개발자가 된 이야기").class("session-title")
+                                        Span("#Swift").class("session-tag")
+                                    }.class("session-info")
+                                    Div{
+                                        Span("GeekCode | Swift 개발자")
+                                    }.class("speaker-info")
+                                }.class("session-describe")
+                            }
+                            TableRow {
+                                TableCell("14:40 ~ 15:10").class("session-time")
+                                TableCell{
+                                    Div {
+                                        Span("Chat GPT 로 iOS App 만들기").class("session-title")
+                                        Span("#ML").class("session-tag")
+                                    }.class("session-info")
+                                    Div{
+                                        Span("With 오거나이저 | Swift, ML 개발자")
+                                    }.class("speaker-info")
+                                }.class("session-describe")
+                            }
+                            TableRow {
+                                TableCell("15:10 ~ 15:40").class("session-time")
+                                TableCell{
+                                    Div {
+                                        Span("쉬는시간").class("session-title")
+                                    }.class("session-info")
+                                }.class("session-describe")
+                            }.class("breaktime")
+                            TableRow {
+                                TableCell("15:40 ~ 16:10").class("session-time")
+                                TableCell{
+                                    Div {
+                                        Span("마! 이게 생산성이다 - 생산성을 위한 CLI 구축").class("session-title")
+                                        Span("#Swift").class("session-tag")
+                                    }.class("session-info")
+                                    Div{
+                                        Span("Jihoon Ahn | Swift 개발자")
+                                    }.class("speaker-info")
+                                }.class("session-describe")
+                            }
+                            TableRow {
+                                TableCell("16:10 ~ 16:40").class("session-time")
+                                TableCell{
+                                    Div {
+                                        Span("Swift Coding Club의 오거나이저").class("session-title")
+                                        Span("#Swift").class("session-tag")
+                                    }.class("session-info")
+                                    Div{
+                                        Span("햄식 | Swift 개발자")
+                                    }.class("speaker-info")
+                                }.class("session-describe")
+                            }
+                            TableRow {
+                                TableCell("16:40 ~ 17:10").class("session-time")
+                                TableCell{
+                                    Div {
+                                        Span("ML on iOS").class("session-title")
+                                        Span("#ML").class("session-tag")
+                                    }.class("session-info")
+                                    Div{
+                                        Span("김현길 or 이준원 | ML 개발자")
+                                    }.class("speaker-info")
+                                }.class("session-describe")
+                            }
+                            TableRow {
+                                TableCell("17:10 ~ 17:20").class("session-time")
+                                TableCell{
+                                    Div {
+                                        Span("쉬는시간").class("session-title")
+                                    }.class("session-info")
+                                }.class("session-describe")
+                            }.class("breaktime")
+                            TableRow {
+                                TableCell("17:20 ~ 17:50").class("session-time")
+                                TableCell{
+                                    Div {
+                                        Span("테크토크- 무엇이든 물어보세요").class("session-title")
+                                        Span("#Talk").class("session-tag")
+                                    }.class("session-info")
+                                    Div{
+                                        Span("진행자: Leeo")
+                                    }.class("speaker-info")
+                                }.class("session-describe")
+                            }
+                            TableRow {
+                                TableCell("18:00 ~ 22:00").class("session-time")
+                                TableCell{
+                                    Div {
+                                        Span("클로징 | 로사").class("session-title")
+                                    }.class("session-info")
+                                }.class("session-describe")
+                            }
                         }
-                        TableRow {
-                            TableCell("14:10 ~ 14:40").class("session-time")
-                            TableCell("기타치다가 Swift 개발자가 된 이야기 / GeekCode").class("session-title")
-                        }
-                        TableRow {
-                            TableCell("14:40 ~ 15:10").class("session-time")
-                            TableCell("Chat-GPT로 iOS앱 만들기 / iOS-iGA").class("session-title")
-                        }
-                        TableRow {
-                            TableCell("15:10 ~ 15:40").class("session-time")
-                            TableCell("Break time & Networking").class("session-title")
-                        }
-                        TableRow {
-                            TableCell("15:40 ~ 16:10").class("session-time")
-                            TableCell("미친 생산성을 위한 CLI 구축 / Jihoon Ahn").class("session-title")
-                        }
-                        TableRow {
-                            TableCell("16:10 ~ 16:40").class("session-time")
-                            TableCell("Swift Coding Club이라는 커뮤니티의 오거나이저를 하면서...(미정) / 햄식(미정)").class("session-title")
-                        }
-                        TableRow {
-                            TableCell("16:40 ~ 17:10").class("session-time")
-                            TableCell("ML on iOS(가칭) / 김현길 or 이준원(미정)").class("session-title")
-                        }
-                        TableRow {
-                            TableCell("17:10 ~ 17:20").class("session-time")
-                            TableCell("Break time").class("session-title")
-                        }
-                        TableRow {
-                            TableCell("17:20 ~ 17:50").class("session-time")
-                            TableCell("세션 발표자와 토킹 세션 / 발표자 일동").class("session-title")
-                        }
-                        TableRow {
-                            TableCell("17:50 ~ 18:00").class("session-time")
-                            TableCell("Closing / Rosa").class("session-title")
-                        }
-                        TableRow {
-                            TableCell("18:00 ~ 22:00").class("session-time")
-                            TableCell("Networking").class("session-title")
-                        }
-                    }
-                ).class("session-table")
+                    ).class("session-table")
+                }.class("session-table-wrapper")
             }.class("sec-timetable")
         }.id("main-timetable").class("sub-sec")
     }
