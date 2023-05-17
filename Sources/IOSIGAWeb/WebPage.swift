e0aba7d62fded47dd891e1f43ed6657f59aab941
choo121600 <choo121600@gmail.com>
2023-05-18 01:55:19 +0900

    og:image test

--- Sources/IOSIGAWeb/WebPage.swift
+++ Sources/IOSIGAWeb/WebPage.swift
@@ -14,12 +14,10 @@ struct WebPage: Website {
     enum SectionID: String, WebsiteSectionID {
         // Add the sections that you want your website to contain here:
         case webPage
-        case posts
         
         var name: String {
             switch self {
             case .webPage: return "WebPage"
-            case .posts: return ""
             }
         }
     }
@@ -29,13 +27,13 @@ struct WebPage: Website {
     }
 
     // Update these properties to configure your website:
-    var url = URL(string: "https://publish-web.vercel.app")!
-    var name = "iOS iGA"
-    var description = "iOS 개발자들을 위한 iOS iGA 2023에서 모두 함께 즐겨보아요."
-    var language: Language { .english }
     var imagePath: Path? {"/static/images/main/web-preview.png"}
     var favicon: Favicon? {
         Favicon(path: "/static/images/main/favicon-32x32.png", type: "image/x-icon")
     }
+    var url = URL(string: "https://publish-web.vercel.app")!
+    var name = "iOS iGA"
+    var description = "iOS 개발자들을 위한 iOS iGA 2023에서 모두 함께 즐겨보아요."
+    var language: Language { .english }
 }
 
