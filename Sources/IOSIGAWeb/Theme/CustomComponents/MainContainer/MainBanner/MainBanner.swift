8a8a8132dae390bc002545e1fcd235b79118b972
choo121600 <choo121600@gmail.com>
2023-06-06 16:10:29 +0900

    Feat: 티켓 오픈 및 버튼 커스텀

--- Sources/IOSIGAWeb/Theme/CustomComponents/MainContainer/MainBanner/MainBanner.swift
+++ Sources/IOSIGAWeb/Theme/CustomComponents/MainContainer/MainBanner/MainBanner.swift
@@ -17,14 +17,10 @@ struct MainBanner: Component {
             Div {
                 Image("/static/images/main/mainBanner.png")
             }.class("sec-banner")
-            Div {
-                Paragraph("페스타를 통해 6월 8일 오픈")
-            }.class("before-open-ticket")
-//            Div {
-//                Link(url: "https://festa.io/events/3506"){
-//                    Image("/static/images/main/ticketBtn.png")
-//                }
-//            }.class("ticketBtn")
+            Link(url: "https://festa.io/events/3615"){
+                Span("티켓 구매")
+                Image("/static/images/main/nextImg.png")
+            }.class("ticketBtn")
             Div {
                 Paragraph {
                     Text("Scroll to Read")
